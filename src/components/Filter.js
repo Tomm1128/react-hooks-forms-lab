6ed54ac3192130bb796d5807f89c40928c43f35a
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-11 10:10:31 -0400

    Create search by food name functionality

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onSearchFood, onCategoryChange }) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" name="search" placeholder="Search..." onChange={onSearchFood} />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,13 +5,24 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchedFood, setSearchFood] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
+  const updateFoodSearch = (event) => {
+    console.log(event.target.value)
+    setSearchFood(event.target.value)
+  }
+
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+    console.log(item)
+    if (searchedFood.length > 0){
+      let foodName = item.name.toLowerCase()
+      return foodName.includes(searchedFood)
+    }
+    else if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
@@ -19,7 +30,7 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onSearchFood={updateFoodSearch} onCategoryChange={handleCategoryChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
